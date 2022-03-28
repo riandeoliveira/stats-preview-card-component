f7bd8e2352f0a9c10ff3620e0569ec0bcff7207c
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-03-28 11:11:37 -0300

    📦 refactor: Update ```App.tsx```

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import data from "./data/data.json";
+import { Data } from "./components/Data";
 import { Card } from "./components/Card";
 
+const { data_names, data_numbers } = data;
+
 export const App: React.FC = () => {
-  return <Card />;
+  const handleCardData = data_names.map((item, i) => (
+    <Data name={data_names[i]} number={data_numbers[i]} />
+  ));
+
+  return <Card cardData={handleCardData} />;
 };
