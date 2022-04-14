9385b972775e61685e4fb8af8ffe9dc9b95ac0e2
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-14 00:47:50 -0300

    📦 refactor: Update ```global-style.ts```

--- src/styles/global/global-style.ts
+++ src/styles/global/global-style.ts
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 import theme from "../theme/theme.json";
-import device from "media-query-sizes";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -23,50 +22,6 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     background-color: ${theme.colors.ebony};
-  }
-
-  @media ${device.laptopL} {
-    .card-container {
-      flex-direction: column;
-
-      .workspace {
-        border-radius: 10px 10px 0 0;
-        width: 100%;
-      }
-
-      .main-area {
-        border-radius: 0 0 10px 10px;
-        max-width: 532px;
-        padding: 40px;
-        text-align: center;
-
-        .description-container {
-          max-width: none;
-
-          .description {
-            margin-bottom: 25px;
-          }
-        }
-
-        .data-container {
-          justify-content: space-around;
-
-          .info-container {
-            margin: 0;
-          }
-        }
-      }
-    }
-  }
-
-  @media ${device.tablet} {
-    .data-container {
-      flex-direction: column;
-
-      .info-container {
-        padding-bottom: 15px;
-      }
-    }
   }
 `;
 
